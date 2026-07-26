cd6c2dd7c803bd0b571cb104046b967e87b8494e
ilhneliff <elifilhan2003@hotmail.com>
2026-07-26 22:39:45 +0300

    hafta-05 g1: vite + react proje kurulumu

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App(){
+  return(
+    <div>
+      <h1>Merhaba, ben Elif'in ilk react uygulaması</h1>
+      <p>Bugün:{new Date().toLocaleDateString('tr-TR')}</p>
+    </div>
+  )
+}
+export default App
